86feb7f6ce67cf5c10441d3acceb7a9b663f02a3
broccolirob <rschn4911@gmail.com>
2020-06-19 11:04:49 -0400

    Test block class

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -2,6 +2,14 @@ import time
 from backend.util.crypto_hash import crypto_hash
 
 
+GENESIS_DATA = {
+    'timestamp': 1,
+    'last_hash': 'genesis_last_hash',
+    'block_hash': 'genesis_hash',
+    'data': []
+}
+
+
 class Block:
     """
     Block: a unit of storage.
@@ -39,7 +47,13 @@ class Block:
         """
         Generate the genesis block.
         """
-        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+        # return Block(
+        #     timestamp=GENESIS_DATA['timestamp'],
+        #     last_hash=GENESIS_DATA['last_hash'],
+        #     block_hash=GENESIS_DATA['block_hash'],
+        #     data=GENESIS_DATA['data']
+        # )
+        return Block(**GENESIS_DATA)
 
 
 def main():
