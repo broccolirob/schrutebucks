1675f34fdda313584472d87bc77b18157bee31a3
broccolirob <rschn4911@gmail.com>
2020-06-25 14:37:44 -0400

    Add pubsub via pubnub to project

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify
 from backend.blockchain.blockchain import Blockchain
+from backend.pubsub import PubSub
 
 app = Flask(__name__)
 blockchain = Blockchain()
-
-for i in range(3):
-    blockchain.add_block(i)
+pubsub = PubSub()
 
 
 @app.route('/')
@@ -16,6 +15,15 @@ def route_default():
 @app.route('/blockchain')
 def route_blockchain():
     return jsonify(blockchain.to_json())
+
+
+@app.route('/blockchain/mine')
+def route_blockchain_mine():
+    transaction_data = 'stubbed_transaction_data'
+
+    blockchain.add_block(transaction_data)
+
+    return jsonify(blockchain.chain[-1].to_json())
 
 
 app.run()
